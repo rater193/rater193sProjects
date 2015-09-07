34461154d8811f73a4560c86bf049e4be0b3a011
rater193 <rater193@gmail.com>
2015-09-07 02:55:54 -0400

    UP Update

--- Opencomputers/OSTest/lib/userinterface.lua
+++ Opencomputers/OSTest/lib/userinterface.lua
@@ -1,14 +1,62 @@
 local gfx = {}
 
 local component = require("component")
+local term = require("term")
+
 local gpu = component.gpu
+
+
+
+-- Function decToHex (renamed, updated): http://lua-users.org/lists/lua-l/2004-09/msg00054.html
+local function decToHex(IN)
+	local B,K,OUT,I,D=16,"0123456789ABCDEF","",0
+	while IN>0 do
+    	I=I+1
+    	IN,D=math.floor(IN/B),math.fmod(IN,B)+1
+    	OUT=string.sub(K,D,D)..OUT
+	end
+	return OUT
+end
+
+-- Function rgbToHex: http://gameon365.net/index.php
 
 function gfx.new()
 	local draw = {}
+		function draw.colorHexToRGB(hex)
+			hex = hex:gsub("#","")
+			return tonumber("0x"..hex:sub(1,2)), tonumber("0x"..hex:sub(3,4)), tonumber("0x"..hex:sub(5,6))
+		end
+
+		function draw.colorRGBToHex(r, g, b)
+			local output = decToHex(r) .. decToHex(g) .. decToHex(b);
+			return output
+		end
 		
+		function draw.getSize()
+			return gpu.getResolution()
+		end
+		
+		function draw.getWidth()
+			local w, h = draw.getSize()
+			return w
+		end
+		
+		function draw.getHeight()
+			local w, h = draw.getSize()
+			return h
+		end
+		
+		function draw.getHeight()
+			
+		end
 		
 		function draw.setColor(...)
-			return gpu.setBackground(...)
+			gpu.setBackground(...)
+			gpu.setForeground(...)
+		end
+		
+		function draw.setColorRGB(r, g, b)
+			return tonumber("0x"..draw.colorRGBToHex(r, g, b))
 		end
 		
 		function draw.getColor(...)
@@ -34,6 +82,11 @@ function gfx.new()
 					draw.pixel(_x, _y)
 				end
 			end
+		end
+		
+		function draw.text(x, y, text)
+			term.setCursor(x, y)
+			term.write(text)
 		end
 		
 		
--- Opencomputers/OSTest/startup.lua
+++ Opencomputers/OSTest/startup.lua
@@ -1,7 +1,41 @@
+local ui = require("userinterface")
+local term = require("term")
+local gpu = require("component").gpu
 
-settings = {}
-settings.term = require("term")
-settings.component = require("component")
-settings.gpu = settings.component.gpu
+local draw = ui.new()
 
-for i, v in pairs(settings.component.gpu) do print(i, v) end
+local oldcolor = draw.getColor()
+
+local fg, bg = gpu.getForeground(), gpu.getBackground()
+
+term.clear()
+
+draw.setColor(0xffffff)
+
+local w, h = draw.getSize()
+
+local _y = math.floor(h/2)
+
+for _place = 0,10 do
+draw.rectangle(0,_y-1,math.ceil((_place/10)*w),_y-1)
+draw.rectangle(0,_y+1,math.ceil((_place/10)*w),_y+1)
+os.sleep(1/30)
+end
+
+draw.setColor(oldcolor)
+gpu.setForeground(0x8888ff)
+draw.text(4,_y,"Password: ")
+
+local pass = io.read()
+
+draw.text(8, _y+2, "Password protection coming soon!")
+
+gpu.setForeground(fg)
+gpu.setBackground(bg)
+
+
+os.sleep(2)
+
+term.clear()
+_G._OSVERSION = "RatOS 0.1"
+print(_G._OSVERSION.." Command line")
